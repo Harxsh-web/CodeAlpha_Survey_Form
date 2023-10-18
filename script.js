7c6ddeffc324bb99e18699dfdddb7abbdbe742ea
Harxsh-web <136264726+Harxsh-web@users.noreply.github.com>
2023-10-18 23:58:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function check(){
+    var data = document.getElementById("name").value;
+    var data1 = document.getElementById("name1").value;
+    var data2 = document.getElementById("name2").value;
+    if (data=="" || data1=="" || data2=="" )
+    {
+        alert("Please Fill all the details");
+    }
+    else{
+        alert("Thank you");
+    }
+    
+}
